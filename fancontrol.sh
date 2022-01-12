f1c7c40014fdd6f0ac460dd410b31ee017cffd95
hazarjast <30390295+hazarjast@users.noreply.github.com>
2022-01-12 17:16:20 -0500

    Update fancontrol.sh

--- fancontrol.sh
+++ fancontrol.sh
@@ -1,15 +1,55 @@
 #!/bin/sh
-# Activate intake and exhaust fans if modem cpu temp exceeds 55c (130f)
+# Activate intake and exhaust fans if modem cpu temp exceeds $LIMIT (in degress celsius)
 
-LOG="/var/log/fan_history"
+PIDFILE=/var/run/fan_control.pid
+LOG=/var/log/fan_control.log
 LIMIT=55
 HUB="1-1.3"
-STATE=$(/usr/sbin/uhubctl -l $HUB | grep -m 1 -q off)
+STATE=$(/usr/sbin/uhubctl -l $HUB | grep -m 1 -q off; echo $?)
 TEMP=$(echo -e AT+QTEMP | socat -t 1 - /dev/ttyUSB2,crnl | grep cpu0-a7-usr | egrep -o "[0-9][0-9]+")
 
-if [ $TEMP -ge $LIMIT ] || [ $STATE -eq 0 ]
+# Preliminary logic to ensure this only runs one instance at a time
+if [ -f $PIDFILE ]
+then
+  PID=$(cat $PIDFILE)
+  ps | awk '{print $1}' | grep -q $PID
+  if [ $? -eq 0 ]
+  then
+    echo "$(date) - Process already running. Exiting." >> $LOG
+    exit 1
+  else
+    echo $$ > $PIDFILE
+    if [ $? -ne 0 ]
+    then
+      echo "$(date) - Could not create PID file. Exiting." >> $LOG
+      exit 1
+    fi
+  fi
+else
+  echo $$ > $PIDFILE
+  if [ $? -ne 0 ]
+  then
+    echo "$(date) - Could not create PID file. Exiting." >> $LOG
+    exit 1
+  fi
+fi
+
+if [ $STATE -eq 0 ] && [ $TEMP -ge $LIMIT ]
 then
   /usr/sbin/uhubctl -l $HUB -a 1 >/dev/null 2>/dev/null
-else
-  echo "placeholder"
+  echo "$(date) -  Modem reached $TEMP which is greater than or equal to the limit of $LIMIT. Fans activated." >> $LOG
+elif [ $STATE -eq 1 ] && [ $TEMP -lt $LIMIT ]
+then
+  /usr/sbin/uhubctl -l $HUB -a 0 >/dev/null 2>/dev/null
+  echo "$(date) -  Modem fell below $TEMP which is less than the limit of $LIMIT. Fans deactivated." >> $LOG
 fi
+
+# Keep log from getting too large
+if [ -f $LOG ]
+then
+  echo "$(tail -1000 $LOG)" > $LOG
+fi
+
+rm $PIDFILE
+
+exit 0
