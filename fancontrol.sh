d80a99492efbef5e81cde0d232154b50c1ba4779
hazarjast <30390295+hazarjast@users.noreply.github.com>
2022-01-13 15:39:58 -0500

    Update fancontrol.sh

--- fancontrol.sh
+++ fancontrol.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
+
+#
 # Activate intake and exhaust fans if modem cpu temp exceeds $LIMIT (in degress celsius)
+# Deactivate fans if modem cpu temp falls below $LIMIT
+#
 
 PIDFILE=/var/run/fan_control.pid
 LOG=/var/log/fan_control.log
 LIMIT=55
 HUB="1-1.3"
+ATDEVICE=/dev/ttyUSB2
+$UHUBCTL=/usr/sbin/uhubctl
 
 # Preliminary logic to ensure this only runs one instance at a time
 if [ -f $PIDFILE ]
@@ -32,32 +38,46 @@ else
   fi
 fi
 
-STATE=$(/usr/sbin/uhubctl -l $HUB | grep -m 1 -q off; echo $?)
-TEMP=$(echo -e AT+QTEMP | socat -W - /dev/ttyUSB2,crnl | grep cpu0-a7-usr | egrep -o "[0-9][0-9]+")
+# Query current fan state from uhubctl
+STATE=$($UHUBCTL -l $HUB | grep -o -m 1 'off\|power')
 
-if [ echo $TEMP | egrep -o "[0-9][0-9]+" ]
+# Query current temperature of modem cpu
+TEMP=$(echo -e AT+QTEMP | socat -W - $ATDEVICE,crnl | grep cpu0-a7-usr | egrep -o "[0-9][0-9]+")
+
+# Check that returned fan state is valid before proceeding; error exit if not.
+if [ $(echo $STATE | egrep -o "off") ]
 then
-  break
+  continue
+else
+  echo "$(date) -  Could not obtain a valid state from the fan; maybe uhubctl is busy. Exiting." >> $LOG
+  exit 1
+fi
+
+# Check that returned modem cpu temp is valid before proceeding; error exit if not.
+if [ $(echo $TEMP | egrep -o "[0-9][0-9]+") ]
+then
+  continue
 else
   echo "$(date) -  Could not obtain a valid cpu temperature from the modem; maybe it is busy. Exiting." >> $LOG
   exit 1
+fi
 
-if [ $STATE -eq 0 ] && [ $TEMP -ge $LIMIT ]
+# Main fan control logic
+if [ $STATE = "off" ] && [ $TEMP -ge $LIMIT ]
 then
-  /usr/sbin/uhubctl -l $HUB -a 1 >/dev/null 2>/dev/null
+  $UHUBCTL -l $HUB -a on >/dev/null 2>/dev/null
   echo "$(date) -  Modem cpu reached $TEMP which is greater than or equal to the limit of $LIMIT. Fans activated." >> $LOG
-elif [ $STATE -eq 1 ] && [ $TEMP -lt $LIMIT ]
+elif [ $STATE = "power" ] && [ $TEMP -lt $LIMIT ]
 then
-  /usr/sbin/uhubctl -l $HUB -a 0 >/dev/null 2>/dev/null
+  $UHUBCTL -l $HUB -a off >/dev/null 2>/dev/null
   echo "$(date) -  Modem cpu cooled to $TEMP which is less than the limit of $LIMIT. Fans deactivated." >> $LOG
 fi
 
-# Keep log from getting too large
+# Houskeeping
 if [ -f $LOG ]
 then
   echo "$(tail -1000 $LOG)" > $LOG
 fi
-
 rm $PIDFILE
 
 exit 0
