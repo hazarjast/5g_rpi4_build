844e6cd9d0e39601d5133dd374c672f55151a2c8
hazarjast <30390295+hazarjast@users.noreply.github.com>
2022-01-18 17:26:00 -0500

    Update fancontrol.sh

--- fancontrol.sh
+++ fancontrol.sh
@@ -5,10 +5,9 @@
 # Activate intake and exhaust fans if modem cpu temp exceeds $LIMIT (in degress celsius).
 # Deactivate fans if modem cpu temp falls below $LIMIT.
 #
-# NOTE: This will disable ModemManager's Hotplug device cleanup script on first run.
-# The Hotplug ModemManager script must be disabled for uhubctl to work properly.
-# A ModemManager udev rule will also be added to unbind a secondary AT port for our use.
-# On first run, both Hotplug and udev changes will be made; a reboot will then be requested.
+# NOTE: A ModemManager udev rule is added at first run to unbind a secondary AT port for our use.
+# User is then prompted to reboot OpenWRT for the change to take effect.
+# Be sure that $MMUBIND is populated with the correct MM USBIFNUM before running this script!
 #
 # Assumptions:
 # Intended to be used with a USB hub which supports Per Port Power Switching (PPPS).
@@ -21,6 +20,8 @@
 #
 
 PIDFILE=/var/run/fan_control.pid
+FANON=/var/run/fan.on
+STATE="off"
 LOG=/var/log/fan_control.log
 LIMIT=55
 HUB="1-1.3"
@@ -28,7 +29,6 @@ ATDEVICE=/dev/ttyUSB3
 MMVID="2c7c"
 MMPID="0800"
 MMUBIND="03"
-REBOOT=0
 HPDIR=/etc/hotplug.d/usb
 
 # Preliminary logic to ensure this only runs one instance at a time
@@ -44,38 +44,13 @@ then
   fi
 else
   echo $$ > $PIDFILE
-  if [ ! -f "$PIDFILE" ] && [ ! $(grep -s $$ $PIDFILE) ]
+  if [ ! -f "$PIDFILE" ]
   then
     echo "$(date) - Could not create PID file. Exiting." >> $LOG
     exit 1
   else
     continue
   fi
-fi
-
-# Check for ModemManager hotplug actions and move them if present
-# This mitigates inconsistent uhubctl behavior when checking fan state
-HPFILE=$(ls $HPDIR | grep modemmanager)
-if [ ! -z $HPFILE ] && [ -f "$HPDIR/$HPFILE" ]
-then
-  mkdir $HPDIR/bak 2>/dev/null
-  if [ ! -d "$HPDIR/bak" ]
-  then
-    echo "$(date) - Could not backup ModemManager Hotplug config backup directory. Exiting." >> $LOG
-    exit 1
-  else
-    mv $HPDIR/$HPFILE $HPDIR/bak/ 2>/dev/null
-    if [ ! -f "$HPDIR/bak/$HPFILE" ]
-    then
-      echo "$(date) - Could not backup ModemManager Hotplug config file. Exiting." >> $LOG
-      exit 1
-    else
-      echo "$(date) - Removed incompatible ModemManager Hotplug config with backup at '$HPDIR/bak/$HPFILE'." >> $LOG
-      REBOOT=$(expr $REBOOT + 1)
-    fi
-  fi
-else
-  continue
 fi
 
 # Unbind ModemManager from the secondary AT port so we can use it
@@ -94,44 +69,17 @@ LABEL="mm_test_end"
 EOF
 
   echo "$(date) - Unbound ModemManager from USBIFNUM $MMUBIND on modem $MMVID:$MMPID." >> $LOG
-  REBOOT=$(expr $REBOOT + 1)
-
-else
-  continue
-fi
-
-# If initial ModemManager Hotplug/udev changes were made, prompt for user reboot
-if [ $REBOOT -gt 0 ]
-then
-  echo "$(date) - ModemManager config changes were made. Prompted user to reboot." >> $LOG
   echo "ModemManager config changes were made. Please reboot OpenWRT before executing this script again."
-  exit 0
+  echo "$(date) - ModemManager config changes were made. Prompted user to reboot." >> $LOG
 else
   continue
 fi
 
-# Query current fan state from uhubctl
-STATE=$(uhubctl -l $HUB | grep -o -m 1 'off\|power')
+# Check current fan state
+[ -f $FANON ] && STATE="power"
 
 # Query current temperature of modem cpu
 TEMP=$(timeout -k 5 5 echo -e AT+QTEMP | socat -W - $ATDEVICE,crnl | grep cpu0-a7-usr | egrep -o "[0-9][0-9]")
-
-# Check that returned fan state is valid, if not, query it again up 5x until it gets a valid result
-# If no valid result returned, exit with error
-TRIES=0
-while [ ! $(echo $STATE | grep -o -m 1 'off\|power') ]
-do
-  STATE=$(uhubctl -l $HUB | grep -o -m 1 'off\|power')
-  sleep 2
-  TRIES=$(expr $TRIES + 1)
-  if [ $TRIES -lt 5 ]
-  then
-    continue
-  else
-    echo "$(date) - Could not obtain a valid state from the fan. Exiting." >> $LOG
-    exit 1
-  fi
-done
 
 # Check that returned modem cpu temp is valid, if not, query it again up 5x until it gets a valid result
 # If no valid result returned, exit with error
@@ -154,10 +102,12 @@ done
 if [ $STATE = "off" ] && [ $TEMP -ge $LIMIT ]
 then
   uhubctl -l $HUB -a on >/dev/null 2>/dev/null
+  touch $FANON
   echo "$(date) - Modem cpu reached $TEMP which is greater than or equal to the limit of $LIMIT. Fans activated." >> $LOG
 elif [ $STATE = "power" ] && [ $TEMP -lt $LIMIT ]
 then
   uhubctl -l $HUB -a off >/dev/null 2>/dev/null
+  rm $FANON
   echo "$(date) - Modem cpu cooled to $TEMP which is less than the limit of $LIMIT. Fans deactivated." >> $LOG
 fi
 
