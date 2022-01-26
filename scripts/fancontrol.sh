990cdffaebab8218e15caac79230b82855d45d28
hazarjast <30390295+hazarjast@users.noreply.github.com>
2022-01-26 11:49:07 -0500

    Update fancontrol.sh

--- scripts/fancontrol.sh
+++ scripts/fancontrol.sh
@@ -58,10 +58,10 @@ FANON=/var/run/fan.on
 case "$PFEXST" in
   "true") PID=$(cat $PIDFILE)
          $(ps | awk '{print $1}' | grep -q $PID) && \
-         $($ERROR "Already running. Exiting." ; exit 1) || \
-         $(echo $$ > $PIDFILE || $ERROR "Could not create PID file. Exiting." ; exit 1)
+         $($ERROR "Already running. Exiting." && exit 1) || \
+         $(echo $$ > $PIDFILE || $ERROR "Could not create PID file. Exiting." && exit 1)
   ;;
-  "false") $(echo $$ > $PIDFILE) || $($ERROR "Could not create PID file. Exiting." ; exit 1)
+  "false") $(echo $$ > $PIDFILE) || $($ERROR "Could not create PID file. Exiting." && exit 1)
   ;;
 esac
 
