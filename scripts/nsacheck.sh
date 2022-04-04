40023ef35cf0a6dc4db25def9d2866d79ae930d1
hazarjast <30390295+hazarjast@users.noreply.github.com>
2022-04-04 11:07:07 -0400

    Create nsacheck.sh

---
+++ scripts/nsacheck.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+#
+# *Script info*
+# Call a speedtest and cache the result. If under 100Mbps disable modem.
+# Disabled modem will then be cycled by modemwatcher.sh (if running).
+# It is necessary to do this as sometimes NSA aggregation becomes inactive.
+#
+# *Assumptions*
+# This script should be scheduled to run 'off-hours' under root's crontab.
+# This script should exist under '/scripts/'.
+# 'pservice' daemon w/ modemwatcher.sh should be running.
+#
+# *Required Inputs*
+# $THRESHOLD - Threshold in Mpbs; a result less than or equal to this will trigger disabling modem.
+#
+# *Dependencies*
+# This script requires 'modemmanager' and 'modemwatcher.sh' to be installed and active.
+# Also requires latest aarch64 version of Ookla's 'speedtest' script to exist under '/usr/bin'.
+# https://www.speedtest.net/apps/cli
+#
+# Copyright 2022 hazarjast (and aliases) - hazarjast at protonmail dot com
+#
+MMCLI="/usr/bin/mmcli"
+MINDEX="$($MMCLI -L -K | egrep -o '/org/freedesktop/.*' | tr -d "'")"
+SPEEDTEST="/usr/bin/speedtest"
+INFO="/usr/bin/logger -t NSA_CHECK"
+THRESHOLD=100
+
+$INFO "Executing speedtest to determine if NSA is active..."
+$SPEEDTEST -p no > /tmp/speedtest.result && \
+[ $(egrep -o 'Download: *\d*' /tmp/speedtest.result | tr -d "Download: ") -gt $THRESHOLD ] || \
+$MMCLI -m $MINDEX -d >/dev/null 2>/dev/null ; DISABLED=1
+
+if [ -z $DISABLED ]
+then
+  $INFO "Speedtest download result less $THRESHOLD Mbps. NSA appears to be inactive."
+  $INFO "Modem was disabled and will be reset momentarily by ModemWatcher."
+else
+  $INFO "Speedtest download result was greater than $THRESHOLD Mbps. NSA appears to be active."
+fi
+
+exit 0
